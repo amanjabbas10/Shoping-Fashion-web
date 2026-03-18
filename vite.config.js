2b847689c8f6011da4eb31eedd69c654829c1f80
amanjabbas <amanjabbas4.7@gmail.com>
2026-03-18 13:56:40 -0700

    adde

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base:  "/Shoping-Fashion-web/" ,
+  base: mode === 'production' ? '/Shoping-Fashion-web/' : '/',
 }))
